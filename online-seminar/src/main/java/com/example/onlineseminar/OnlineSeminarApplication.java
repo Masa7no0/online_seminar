01f3ba7575c66f1e737531f39e55cb272121f9f4
ooguikarateka <92349075+ooguikarateka@users.noreply.github.com>
2021-11-17 11:54:59 +0900

    Add files via upload

---
+++ online-seminar/src/main/java/com/example/onlineseminar/OnlineSeminarApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineseminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSeminarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineSeminarApplication.class, args);
+    }
+
+}
---
+++ online-seminar/src/test/java/com/example/onlineseminar/OnlineSeminarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineseminar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineSeminarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
